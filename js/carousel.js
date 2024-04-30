3245f5681830571b2d4fa88a86951353ec75bf0f
Giorgia Pirovano <162731896+Giorgia46@users.noreply.github.com>
2024-04-30 09:44:35 +0200

    file-js-carosello

---
+++ js/carousel.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+      perPage: 5, 
+      rewind: true, // Abilita il riavvolgimento infinito
+      breakpoints: {
+        576: {
+          perPage: 1, 
+        },
+
+        768: {
+          perPage: 2, 
+        },
+
+        992: {
+          perPage: 3, 
+        },
+
+        1320: {
+          perPage: 4, 
+        },
+
+
+      }
+    }).mount();
+  });
