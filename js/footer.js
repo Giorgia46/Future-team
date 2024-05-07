6562edb943d6d61ff012f527cb70a97b8c41cf5e
lfossati12 <l.fossati12@campus.unimib.it>
2024-05-07 12:42:32 +0200

    divisione js in file diversi e footer aggiornato

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+//////////////////////////////////////// FOOTER //////////////////////////////////////////////
+
+// Newsletter 
+function subscribe() {
+    var emailInput = $(".form-control"); 
+    var successMessage = $("#successMessage");
+
+    emailInput.val("");
+
+    successMessage.removeClass("d-none").fadeIn(300);
+    setTimeout(function() {
+        successMessage.fadeOut(500, function() {
+            successMessage.addClass("d-none");
+        });
+    }, 3000);
+}
