29970406587dd856dba0027d1cbef8e87ca0d13d
francescamaggiolini <162732116+francescamaggiolini@users.noreply.github.com>
2024-05-08 15:40:25 +0200

    Update portfolio_button.js
    file js aggiornato con tutti i bottoni e schede specifiche del prodotto.

--- js/portfolio_button.js
+++ js/portfolio_button.js
@@ -1,20 +1,171 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    // Bottone e rettangolo 1
+
     const specificheBtn1 = document.getElementById("specificheBtn1");
     const rect1 = document.getElementById("rect1");
     const closeBtn1 = document.getElementById("closeBtn1");
 
-    specificheBtn1.addEventListener("click", function() {
+    specificheBtn1.addEventListener("click", function () {
         specificheBtn1.style.display = "none"; // Nasconde il bottone quando viene cliccato
         rect1.style.display = "block"; // Mostra il rettangolo
         rect1.classList.add("visible");
 
     });
 
-    closeBtn1.addEventListener("click", function() {
+    closeBtn1.addEventListener("click", function () {
         specificheBtn1.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
         rect1.style.display = "none"; // Nasconde il rettangolo
         rect1.classList.remove("visible");
     });
+
+
+    // Bottone e rettangolo 2
+
+    const specificheBtn2 = document.getElementById("specificheBtn2");
+    const rect2 = document.getElementById("rect2");
+    const closeBtn2 = document.getElementById("closeBtn2");
+
+    specificheBtn2.addEventListener("click", function () {
+        specificheBtn2.style.display = "none"; // Nasconde il bottone quando viene cliccato
+        rect2.style.display = "block"; // Mostra il rettangolo
+        rect2.classList.add("visible");
+
+    });
+
+    closeBtn2.addEventListener("click", function () {
+        specificheBtn2.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
+        rect2.style.display = "none"; // Nasconde il rettangolo
+        rect2.classList.remove("visible");
+    });
+
+
+    // Bottone e rettangolo 3
+
+    const specificheBtn3 = document.getElementById("specificheBtn3");
+    const rect3 = document.getElementById("rect3");
+    const closeBtn3 = document.getElementById("closeBtn3");
+
+    specificheBtn3.addEventListener("click", function () {
+        specificheBtn3.style.display = "none"; // Nasconde il bottone quando viene cliccato
+        rect3.style.display = "block"; // Mostra il rettangolo
+        rect3.classList.add("visible");
+
+    });
+
+    closeBtn3.addEventListener("click", function () {
+        specificheBtn3.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
+        rect3.style.display = "none"; // Nasconde il rettangolo
+        rect3.classList.remove("visible");
+    });
+
+
+
+    // Bottone e rettangolo 4
+    const specificheBtn4 = document.getElementById("specificheBtn4");
+    const rect4 = document.getElementById("rect4");
+    const closeBtn4 = document.getElementById("closeBtn4");
+
+    specificheBtn4.addEventListener("click", function () {
+        specificheBtn4.style.display = "none"; // Nasconde il bottone quando viene cliccato
+        rect4.style.display = "block"; // Mostra il rettangolo
+        rect4.classList.add("visible");
+
+    });
+
+    closeBtn4.addEventListener("click", function () {
+        specificheBtn4.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
+        rect4.style.display = "none"; // Nasconde il rettangolo
+        rect4.classList.remove("visible");
+    });
+
+
+
+    // Bottone e rettangolo 5
+    const specificheBtn5 = document.getElementById("specificheBtn5");
+    const rect5 = document.getElementById("rect5");
+    const closeBtn5 = document.getElementById("closeBtn5");
+
+    specificheBtn5.addEventListener("click", function () {
+        specificheBtn5.style.display = "none"; // Nasconde il bottone quando viene cliccato
+        rect5.style.display = "block"; // Mostra il rettangolo
+        rect5.classList.add("visible");
+
+    });
+
+    closeBtn5.addEventListener("click", function () {
+        specificheBtn5.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
+        rect5.style.display = "none"; // Nasconde il rettangolo
+        rect5.classList.remove("visible");
+    });
+
+
+
+
+    // Bottone e rettangolo 6
+    const specificheBtn6 = document.getElementById("specificheBtn6");
+    const rect6 = document.getElementById("rect6");
+    const closeBtn6 = document.getElementById("closeBtn6");
+
+    specificheBtn6.addEventListener("click", function () {
+        specificheBtn6.style.display = "none"; // Nasconde il bottone quando viene cliccato
+        rect6.style.display = "block"; // Mostra il rettangolo
+        rect6.classList.add("visible");
+
+    });
+
+    closeBtn6.addEventListener("click", function () {
+        specificheBtn6.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
+        rect6.style.display = "none"; // Nasconde il rettangolo
+        rect6.classList.remove("visible");
+    });
+
+
+
+    // Bottone e rettangolo 7
+    const specificheBtn7 = document.getElementById("specificheBtn7");
+    const rect7 = document.getElementById("rect7");
+    const closeBtn7 = document.getElementById("closeBtn7");
+
+    specificheBtn7.addEventListener("click", function () {
+        specificheBtn7.style.display = "none"; // Nasconde il bottone quando viene cliccato
+        rect7.style.display = "block"; // Mostra il rettangolo
+        rect7.classList.add("visible");
+
+    });
+
+    closeBtn7.addEventListener("click", function () {
+        specificheBtn7.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
+        rect7.style.display = "none"; // Nasconde il rettangolo
+        rect7.classList.remove("visible");
+    });
+
+
+
+    // Bottone e rettangolo 8
+    const specificheBtn8 = document.getElementById("specificheBtn8");
+    const rect8 = document.getElementById("rect8");
+    const closeBtn8 = document.getElementById("closeBtn8");
+
+    specificheBtn8.addEventListener("click", function () {
+        specificheBtn8.style.display = "none"; // Nasconde il bottone quando viene cliccato
+        rect8.style.display = "block"; // Mostra il rettangolo
+        rect8.classList.add("visible");
+
+    });
+
+    closeBtn8.addEventListener("click", function () {
+        specificheBtn8.style.display = "block"; // Riappare il bottone quando viene chiuso il rettangolo
+        rect8.style.display = "none"; // Nasconde il rettangolo
+        rect8.classList.remove("visible");
+    });
+
+
+
+
+
+
 });
 
 
